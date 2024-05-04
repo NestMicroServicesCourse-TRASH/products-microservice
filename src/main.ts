8f219ed24536e0c35df09d3e759f48b2b0136523
Emmanuel Silva <emanuelsilvadiaz03@gmail.com>
2024-05-04 09:54:41 -0500

    Nats server configuration

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,11 @@ async function bootstrap() {
 
 	const logger = new Logger('ProductsMain');
 
-	const app = await NestFactory.createMicroservice<MicroserviceOptions>
-		(AppModule, {
-			transport: Transport.TCP,
+	const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,
+		{
+			transport: Transport.NATS,
 			options: {
-				port: envs.port,
+				servers: envs.natsServers
 			}
 		});
 
